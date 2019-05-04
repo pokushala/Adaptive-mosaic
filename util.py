8fdf701204790921fe470d4ae4d4e56af612d28f
KernelA <KernelA@users.noreply.github.com>
2019-05-04 22:54:10 +0300

    RGB to HSI conversion improved

--- util.py
+++ util.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def rgb2hsi(rgb: np.ndarray) -> np.ndarray:
     """Conversion RGB to HSI
 
@@ -17,19 +16,26 @@ def rgb2hsi(rgb: np.ndarray) -> np.ndarray:
                                                          "Expected (..., ..., 3) got {}.".format(rgb.shape)
     assert ((rgb >= 0) & (rgb <= 1)).all(), "Color components must be in [0; 1]."
 
-    i = rgb.mean(axis=2)
-    s = 1 - 1 / i * rgb.min(axis=2)
-    rg_diff = rgb[..., 0] - rgb[..., 1]
-    rb_diff = rgb[..., 0] - rgb[..., 2]
-    gb_diff = rgb[..., 1] - rgb[..., 2]
+    all_color_equal = np.all(rgb == rgb[..., 0, np.newaxis], axis=-1)
+    not_all_color_equal = ~all_color_equal
 
-    h = np.arccos(0.5 * (rg_diff + rb_diff) / np.sqrt(rg_diff ** 2 + rb_diff * gb_diff))
-    cond = rgb[..., 2] > rgb[..., 1]
+    i = rgb.mean(axis=-1)
+    s = np.zeros_like(i)
+    h = np.zeros_like(i)
+
+    s[not_all_color_equal] = 1 - 1 / i[not_all_color_equal] * rgb[not_all_color_equal, ...].min(axis=-1)
+    rg_diff = rgb[not_all_color_equal, 0] - rgb[not_all_color_equal, 1]
+    rb_diff = rgb[not_all_color_equal, 0] - rgb[not_all_color_equal, 2]
+    gb_diff = rgb[not_all_color_equal, 1] - rgb[not_all_color_equal, 2]
+
+    h[not_all_color_equal] = np.arccos(0.5 * (rg_diff + rb_diff) / np.sqrt(rg_diff ** 2 + rb_diff * gb_diff))
+    cond = (rgb[..., 2] > rgb[..., 1]) & not_all_color_equal
     h[cond] = 2 * np.pi - h[cond]
 
     h = np.clip(h, 0, 2 * np.pi)
     s = np.clip(s, 0, 1)
     i = np.clip(i, 0, 1)
+    h[all_color_equal] = np.nan
 
     return np.dstack((h, s, i))
 
@@ -54,14 +60,17 @@ def hsi2rgb(hsi: np.ndarray) -> np.ndarray:
     i = hsi[..., 2]
     x = i * (1 - hsi[..., 1])
 
-    cond1 = h < np.pi * 2 / 3
+    h_nan = np.isnan(h)
+    not_h_nan = ~h_nan
+
+    cond1 = np.less(h, np.pi * 2 / 3, where=not_h_nan)
     y = np.zeros_like(h)
     z = np.zeros_like(h)
 
     y[cond1] = i[cond1] * (1 + s[cond1] * np.cos(h[cond1]) / np.cos(np.pi / 3 - h[cond1]))
     z[cond1] = 3 * i[cond1] - (x[cond1] + y[cond1])
 
-    rest = h >= 4 * np.pi / 3
+    rest = np.greater_equal(h, 4 * np.pi / 3, where=not_h_nan)
 
     h[rest] -= 4 * np.pi / 3
     y[rest] = i[rest] * (1 + s[rest] * np.cos(h[rest]) / np.cos(np.pi / 3 - h[rest]))
@@ -81,32 +90,10 @@ def hsi2rgb(hsi: np.ndarray) -> np.ndarray:
     res[rest] = np.roll(res[rest], shift=1, axis=-1)
 
     # Fill NaN value of intensity value
-    where_nan = np.isnan(h)
-
-    res[where_nan] = i[where_nan].reshape(-1, 1)
+    res[h_nan] = i[h_nan].reshape(-1, 1)
 
     return np.clip(res, 0, 1)
 
-
-
-
-if __name__ == "__main__":
-    colors = np.linspace((0, 0, 0), (1, 1, 1), num=250).reshape(-1, 1, 3)
-    #colors = np.random.sample((10000, 100, 3))
-
-    # print(colors)
-
-    hsi = rgb2hsi(colors)
-
-    print(hsi)
-
-    rec_rgb = hsi2rgb(hsi)
-
-    # print(rec_rgb)
-
-    if not np.allclose(colors, rec_rgb):
-        print("Error")
-        print("!" * 10)
 
 
 
